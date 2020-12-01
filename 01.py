daad4e5aa4d9ec5ebf6d97188dc470f5c77e2aff
sleeplessghost <sleeplessghost@live.com>
2020-12-01 21:34:34 +1030

    improvement

--- 01.py
+++ 01.py
@@ -1,23 +1,20 @@
-numbers = []
-for n in open('in/01.txt'):
-    numbers.append(int(n))
+numbers = [int(n) for n in open('in/01.txt')]
 
 def calcA(numbers):
     for i, a in enumerate(numbers):
         for j, b in enumerate(numbers):
-            if i == j: continue
+            if j <= i: continue
             if a + b == 2020: return a * b
     return -1
 
 def calcB(numbers):
     for i, a in enumerate(numbers):
         for j, b in enumerate(numbers):
-            if i == j: continue
+            if j <= i: continue
             for k, c in enumerate(numbers):
-                if i == k or j == k: continue
+                if k <= i or k <= j: continue
                 if a + b + c == 2020: return a * b * c
     return -1
-
 
 print('two numbers sum to 2020 gives: ', calcA(numbers))
 print('three numbers sum to 2020 gives: ', calcB(numbers))
