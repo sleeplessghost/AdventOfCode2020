5b9f7cb832c74783140732a38f9001a0c935f4e0
sleeplessghost <sleeplessghost@live.com>
2020-12-03 09:25:44 +1030

    switch to using regex

--- 02.py
+++ 02.py
@@ -1,15 +1,14 @@
-lines = [line for line in open('in/02.txt')]
+import re
+
+def parse(line) -> (int, int, str, str):
+    parsed = re.search("(?P<a>[0-9]+)-(?P<b>[0-9]+) (?P<letter>[a-zA-Z]): (?P<password>[a-zA-Z]+)", line)
+    return (int(parsed.group('a')), int(parsed.group('b')), parsed.group('letter'), parsed.group('password'))
 
 def countA(lines):
     count = 0
     for line in lines:
-        parts = line.split()
-        range = parts[0].split('-')
-        min = int(range[0])
-        max = int(range[1])
-        letter = parts[1].replace(':','')
-        pw = parts[2]
-        pwCount = pw.count(letter)
+        (min, max, letter, password) = parse(line)
+        pwCount = password.count(letter)
         if (pwCount >= min and pwCount <= max):
             count += 1
     return count
@@ -17,17 +16,13 @@ def countA(lines):
 def countB(lines):
     count = 0
     for line in lines:
-        parts = line.split()
-        range = parts[0].split('-')
-        a = int(range[0]) -1
-        b = int(range[1]) -1
-        letter = parts[1].replace(':','')
-        pw = parts[2]
-        if (a > len(pw) or b > len(pw)):
-            continue
-        if ((pw[a] == letter or pw[b] == letter) and (pw[a] != pw[b])):
+        (a, b, letter, password) = parse(line)
+        a -= 1
+        b -= 1
+        if ((password[a] == letter or password[b] == letter) and (password[a] != password[b])):
             count += 1
     return count
 
+lines = [line for line in open('in/02.txt')]
 print(countA(lines))
 print(countB(lines))
