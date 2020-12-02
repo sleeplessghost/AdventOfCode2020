95b9e18f8befd932eff154f994f6e206074758ee
sleeplessghost <sleeplessghost@live.com>
2020-12-03 10:14:02 +1030

    add better methods

--- 02.py
+++ 02.py
@@ -1,4 +1,5 @@
 import re
+from typing import Callable
 
 def parse(line) -> (int, int, str, str):
     parsed = re.match("(?P<a>[0-9]+)-(?P<b>[0-9]+) (?P<letter>[a-zA-Z]): (?P<password>[a-zA-Z]+)", line)
@@ -22,6 +23,20 @@ def countB(lines):
             count += 1
     return count
 
+def isValidLetterCount(min, max, letter, password):
+    return password.count(letter) in range(min, max + 1)
+
+def isValidIndex(a, b, letter, password):
+    i,j = a - 1, b - 1
+    return password[i] != password[j] and (password[i] == letter or password[j] == letter)
+
+def getValidPWCount(lines, isValid: Callable):
+    return sum(isValid(*parse(line)) for line in lines)
+
 lines = [line for line in open('in/02.txt')]
+
 print('valid PW by letter count:', countA(lines))
 print('valid PW by index:', countB(lines))
+
+print('valid PW by letter count:', getValidPWCount(lines, isValidLetterCount))
+print('valid PW by index:', getValidPWCount(lines, isValidIndex))
