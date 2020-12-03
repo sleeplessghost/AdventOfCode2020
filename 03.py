fa830137bb46c6586709fa4656d037507051fca6
sleeplessghost <sleeplessghost@live.com>
2020-12-03 22:39:04 +1030

    tidying

--- 03.py
+++ 03.py
@@ -1,34 +1,25 @@
-from math import prod
 from copy import deepcopy
 
 lines = [line.strip() for line in open('in/03.txt') if line]
-orig = []
+map = []
 
 for row, line in enumerate(lines):
-    orig.append([])
-    for i, c in enumerate(line):
-        orig[row].append(c)
+    map.append([])
+    for c in line:
+        map[row].append(c)
 
-def checkFor(xStep, yStep):
-    y, x = 0, 0
-    treeCount = 0
-    box = deepcopy(orig)
-    while y < len(box):
-        if box[y][x] == '#': 
-            treeCount += 1
+def checkFor(dx, dy):
+    y, x, trees, length = 0, 0, 0, len(map[0])
+    while y < len(map):
+        if map[y][x] == '#': trees += 1
+        y += dy
+        x  = (x + dx) % length
+    return trees
 
-        x += xStep
-        y += yStep
-
-        if x >= len(box[0]): x -= len(box[0])
-    return treeCount
-
-part1 = checkFor(3,1)
 a = checkFor(1,1)
 b = checkFor(3,1)
 c = checkFor(5,1)
 d = checkFor(7,1)
 e = checkFor(1,2)
-part2 = prod((a,b,c,d,e))
-print('part1:', part1)
-print('part2:', part2)
+print('part1:', checkFor(3,1))
+print('part2:', a * b * c * d * e)
