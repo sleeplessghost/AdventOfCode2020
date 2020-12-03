9f9297b6337bf5f5c876af36e1651f5fdf0c85ff
sleeplessghost <sleeplessghost@live.com>
2020-12-03 23:50:07 +1030

    rename

--- 03.py
+++ 03.py
@@ -1,17 +1,17 @@
 map = [line.strip() for line in open('in/03.txt') if line]
 
-def checkFor(dx, dy):
-    y, x, trees, length = 0, 0, 0, len(map[0])
-    while y < len(map):
+def countTrees(dx, dy):
+    y, x, trees, lengthY, lengthX = 0, 0, 0, len(map), len(map[0])
+    while y < lengthY:
         if map[y][x] == '#': trees += 1
         y += dy
-        x  = (x + dx) % length
+        x  = (x + dx) % lengthX
     return trees
 
-a = checkFor(1,1)
-b = checkFor(3,1)
-c = checkFor(5,1)
-d = checkFor(7,1)
-e = checkFor(1,2)
-print('part1:', checkFor(3,1))
+a = countTrees(1,1)
+b = countTrees(3,1)
+c = countTrees(5,1)
+d = countTrees(7,1)
+e = countTrees(1,2)
+print('part1:', countTrees(3,1))
 print('part2:', a * b * c * d * e)
