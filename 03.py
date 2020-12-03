6b1fe11bb6e73f433c2e1e96da6fa954e06d5f29
sleeplessghost <sleeplessghost@live.com>
2020-12-03 23:56:36 +1030

    tidy

--- 03.py
+++ 03.py
@@ -1,6 +1,4 @@
-map = [line.strip() for line in open('in/03.txt') if line]
-
-def countTrees(dx, dy):
+def countTrees(dx, dy, map):
     y, x, trees, lengthY, lengthX = 0, 0, 0, len(map), len(map[0])
     while y < lengthY:
         if map[y][x] == '#': trees += 1
@@ -8,10 +6,12 @@ def countTrees(dx, dy):
         x  = (x + dx) % lengthX
     return trees
 
-a = countTrees(1,1)
-b = countTrees(3,1)
-c = countTrees(5,1)
-d = countTrees(7,1)
-e = countTrees(1,2)
-print('part1:', countTrees(3,1))
+map = [line.strip() for line in open('in/03.txt')]
+
+print('part1:', countTrees(3,1, map))
+a = countTrees(1,1, map)
+b = countTrees(3,1, map)
+c = countTrees(5,1, map)
+d = countTrees(7,1, map)
+e = countTrees(1,2, map)
 print('part2:', a * b * c * d * e)
