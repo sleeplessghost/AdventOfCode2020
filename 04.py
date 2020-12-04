b6fcdc68bfe329ee6a17e33c4956cf455dcd36e3
sleeplessghost <sleeplessghost@live.com>
2020-12-05 02:06:35 +1030

    str.isdigit lol

--- 04.py
+++ 04.py
@@ -13,15 +13,15 @@ def passportIsValid(passport):
         all(fieldIsValid(field, passport[field]) for field in REQUIRED_FIELDS))
 
 def fieldIsValid(name, value):
-    if (name == 'byr'): return str.isdigit(value) and 1920 <= int(value) <= 2002
-    elif (name == 'iyr'): return str.isdigit(value) and 2010 <= int(value) <= 2020
-    elif (name == 'eyr'): return str.isdigit(value) and 2020 <= int(value) <= 2030
+    if (name == 'byr'): return value.isdigit() and 1920 <= int(value) <= 2002
+    elif (name == 'iyr'): return value.isdigit() and 2010 <= int(value) <= 2020
+    elif (name == 'eyr'): return value.isdigit() and 2020 <= int(value) <= 2030
     elif (name == 'hcl'): return re.match(r'^#[a-fA-F0-9]{6}$', value) is not None
     elif (name == 'ecl'): return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
-    elif (name == 'pid'): return str.isdigit(value) and len(value) == 9
+    elif (name == 'pid'): return value.isdigit() and len(value) == 9
     elif (name == 'hgt'):
         height, units = value[:-2], value[-2:]
-        return str.isdigit(height) and (
+        return height.isdigit() and (
             (units == 'cm' and 150 <= int(height) <= 193) or
             (units == 'in' and 59 <= int(height) <= 76))
 
