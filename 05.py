a52545a1fd8f9a2c5a578403b7695f55eeb4f952
sleeplessghost <sleeplessghost@live.com>
2020-12-05 16:09:27 +1030

    tidy up

--- 05.py
+++ 05.py
@@ -1,28 +1,11 @@
-import re
+def lineToBinary(line):
+    return line.replace('B','1').replace('F','0').replace('R','1').replace('L','0')
 
+lines = [line.strip() for line in open('in/05.txt')]
+seats = [int(lineToBinary(line), 2) for line in lines]
 
-lines = [line.strip() for line in open('in/05.txt') if line]
+possibleSeats = range(0, int('1111111111', 2))
+missingSeat = next(s for s in possibleSeats if (s not in seats and s-1 in seats and s+1 in seats))
 
-max = 0
-seats = []
-for line in lines:
-    fb, rl = line[:-3], line[-3:]
-    binary = '0b' + fb.replace('B','1').replace('F','0')
-    row = int(binary, 2)
-    rlbin = '0b' + rl.replace('R', '1').replace('L', '0')
-    col = int(rlbin, 2)
-    seat = (row * 8) + col
-    if (seat > max): max = seat
-    seats.append(seat)
-
-print(max)
-
-possibleseats = []
-for row in range(0, 128):
-    for col in range(0, 8):
-        possibleseats.append((row * 8) + col)
-
-ls = list(seats)
-for s in possibleseats:
-    if s not in ls and s-1 in ls and s+1 in ls:
-        print(s)
+print('part1:', max(seats))
+print('part2:', missingSeat)
