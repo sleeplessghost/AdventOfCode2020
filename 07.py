359eb98ac10a22d07d4f0adfdb0cae03f587e6b4
sleeplessghost <sleeplessghost@live.com>
2020-12-07 22:24:35 +1030

    rewrite and simplify

--- 07.py
+++ 07.py
@@ -1,52 +1,37 @@
+import re
 
-def canContainGold(name, dictionary, resultDict):
-    if name == 'shiny gold': return True
-    if name == 'no other bags': return False
+def makeDictionary(lines):
+    dictionary = {}
+    for line in lines:
+        colour = re.search(r'(.+) bags contain', line)[1]
+        dictionary[colour] = {}
+        for amount, innerColour in re.findall(r'(\d) (\w+ \w+)', line):
+            dictionary[colour][innerColour] = int(amount)
+    return dictionary
 
-    if name in resultDict: return resultDict[name]
+def contains(current, dictionary, results, target):
+    if current in results: return results[current]
+    if target in dictionary[current].keys():
+        results[current] = True
+        return True
+    inside = any(
+        contains(innerColour, dictionary, results, target) 
+        for innerColour in dictionary[current].keys())
+    results[current] = inside
+    return inside
 
-    subtypes = dictionary[name].keys()
-    result = any(canContainGold(containedType, dictionary, resultDict) for containedType in subtypes)
-    resultDict[name] = result
-    return result
+def countInside(name, dictionary):
+    return sum(
+        amount + (amount * countInside(colour, dictionary))
+        for colour, amount in dictionary[name].items())
 
-def countSubBags(name, dictionary):
-    subdict = dictionary[name]    
+dictionary = makeDictionary(open('in/07.txt'))
+containsDict = {}
 
-    count = 1
-    for key in subdict.keys():
-        count += subdict[key] * countSubBags(key, dictionary)
-    
-    return count
+part1 = sum(
+    contains(name, dictionary, containsDict, 'shiny gold')
+    for name in dictionary.keys() if name != 'shiny gold')
+part2 = countInside('shiny gold', dictionary)
 
-dictionary = {}
-resultDict = {}
-countDict = {}
-lines = [line.strip() for line in open('in/07.txt')]
-
-for line in lines:
-    parts = line.split(' bags contain ')
-    typ = parts[0]
-    ls = parts[1].replace('.','')
-    thisdict = {}
-    if typ in dictionary: thisdict = dictionary[typ]
-    #if type == 'shiny gold': continue
-    
-    if ls != 'no other bags':
-        for x in ls.split(', '):
-            sp = x.split(' ', 1)
-            num = int(sp[0])
-            name = sp[1].replace(' bags','').replace(' bag','')
-
-            thisdict[name] = num
-    
-    dictionary[typ] = thisdict
-
-#test = sum(canContainGold(name, dictionary, resultDict) for name in dictionary.keys())
-#print(sum(resultDict.values()))
-# print(dictionary)
-# print(resultDict)
-
-print(countSubBags('shiny gold', dictionary) - 1)
-# for key in dictionary.keys():
-#     print(key, countSubBags(key, dictionary))
+print('part1:', part1)
+print('part2:', part2)
