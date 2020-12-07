79b350667363251769f5f2e5ff8717b5b42a298d
sleeplessghost <sleeplessghost@live.com>
2020-12-07 23:56:55 +1030

    dont actually need this .keys()

--- 07.py
+++ 07.py
@@ -11,10 +11,10 @@ def makeDictionary(lines):
 
 def contains(name, dictionary, results, target):
     if name not in results:
-        containsTarget = target in dictionary[name].keys()
+        containsTarget = target in dictionary[name]
         results[name] = containsTarget or any(
             contains(innerColour, dictionary, results, target) 
-            for innerColour in dictionary[name].keys())
+            for innerColour in dictionary[name])
     return results[name]
 
 def countInside(name, dictionary, counts):
@@ -31,7 +31,7 @@ target = 'shiny gold'
 
 part1 = sum(
     contains(name, dictionary, containsDict, target)
-    for name in dictionary.keys() if name != target)
+    for name in dictionary if name != target)
 part2 = countInside(target, dictionary, countsDict)
 
 print('part1:', part1)
