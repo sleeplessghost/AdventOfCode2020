5cedf626fad857012b222962491b43860f7f77c9
sleeplessghost <sleeplessghost@live.com>
2020-12-10 00:46:02 +1030

    meh 100ms vs 50ms i dont care
    Revert "use enumerate instead of range"
    
    This reverts commit 03b0643df42ba0adf56fe20f1d6cf9a358f5c4c7.

--- 09.py
+++ 09.py
@@ -10,17 +10,16 @@ def findInvalid(numbers, pSize):
         else: return n
 
 def findRange(numbers, target):
-    for i, valueA in enumerate(numbers):
-        total = valueA
-        for j, valueB in enumerate(numbers[i+1:], i+1):
-            total += valueB
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            total = sum(numbers[i : j+1])
             if total > target: break
             if total == target: return numbers[i : j+1]
 
 numbers = [int(n) for n in open('in/09.txt')]
 
 invalid = findInvalid(numbers, 25)
-contain = findRange(numbers, invalid)
+containing = findRange(numbers, invalid)
 
 print('part1:', invalid)
-print('part2:', min(contain) + max(contain))
+print('part2:', min(containing) + max(containing))
