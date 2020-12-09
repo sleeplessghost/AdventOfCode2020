9aaebd16b9d5dff74e6119f98b70574220fe0dde
sleeplessghost <sleeplessghost@live.com>
2020-12-09 22:41:06 +1030

    tidy

--- 09.py
+++ 09.py
@@ -1,37 +1,29 @@
 from itertools import combinations
 
+def findInvalid(numbers, pSize):
+    preamble = []
+    for n in numbers:
+        if len(preamble) < pSize:
+            preamble.append(n)
+        else:
+            combs = combinations(preamble, 2)
+            if not any(sum(seq) == n for seq in combs):
+                return n
+            else:
+                preamble = preamble[1:]
+                preamble.append(n)
+
+def findRange(numbers, target):
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            total = sum(numbers[i : j+1])
+            if total > target: break
+            if total == target: return numbers[i : j+1]
 
 numbers = [int(n) for n in open('in/09.txt')]
 
-preamble = []
-nf = None
-for n in numbers:
-    if len(preamble) < 25:
-        preamble.append(n)
-    else:
-        combs = combinations(preamble, 2)
-        if not any(sum(seq) == n for seq in combs):
-            nf = n
-            break
-        preamble = preamble[1:]
-        preamble.append(n)
+part1 = findInvalid(numbers, 25)
+containing = findRange(numbers, part1)
 
-print(nf)
-rs = 0
-re = 0
-for i in range(len(numbers)):
-    sum = numbers[i]
-    for j in range(i+1, len(numbers)):
-        sum += numbers[j]
-        if sum == nf:
-            print('range:', numbers[i], numbers[j])
-            rs = i
-            re = j
-        elif sum > nf:
-            break
-
-r = numbers[rs:re+1]
-print(rs, re)
-print(r)
-print(min(r), max(r))
-print(min(r) + max(r))
+print('part1:', part1)
+print('part2:', min(containing) + max(containing))
