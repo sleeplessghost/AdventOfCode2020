1c6cfa8755c83b7e5b04eb7d950836368957c905
sleeplessghost <sleeplessghost@live.com>
2020-12-11 15:49:39 +1030

    day11 part1

---
+++ 11.py
@@ -0,0 +1,49 @@
+from copy import deepcopy
+
+def checkOccupied(row, column, seats):
+    maxX = len(seats[0])
+    maxY = len(seats)
+    if column < 0 or column >= maxX: return False
+    if row < 0 or row >= maxY: return False
+    return seats[row][column] == '#'
+
+def step(seats):
+    result = deepcopy(seats)
+    for row in range(len(seats)):
+        for column in range(len(seats[0])):
+            left = checkOccupied(row, column - 1, seats)
+            right = checkOccupied(row, column + 1, seats)
+            up = checkOccupied(row - 1, column, seats)
+            down = checkOccupied(row + 1, column, seats)
+            UL = checkOccupied(row - 1, column - 1, seats)
+            UR = checkOccupied(row - 1, column + 1, seats)
+            DL = checkOccupied(row + 1, column - 1, seats)
+            DR = checkOccupied(row + 1, column + 1, seats)
+            count = sum([left, right, up, down, UL, UR, DL, DR])
+
+            if seats[row][column] == 'L' and count == 0: result[row][column] = '#'
+            if seats[row][column] == '#' and count >= 4: result[row][column] = 'L'
+    return result
+
+def isSame(a, b):
+    for row in range(len(a)):
+        for col in range(len(a[0])):
+            if a[row][col] != b[row][col]: return False
+    return True
+
+seats = [list(line.strip()) for line in open('in/11.txt')]
+
+old = seats
+new = step(seats)
+while not isSame(old, new):
+    old = new
+    new = step(new)
+
+count = 0
+for row in range(len(new)):
+        for col in range(len(new[0])):
+            if new[row][col] == '#': count += 1
+
+print(count)
+# for row in seats:
+#     print(''.join(row))
