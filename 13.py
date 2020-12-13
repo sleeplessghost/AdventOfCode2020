afe442e71983e5daef96d840da521996dd46193c
sleeplessghost <sleeplessghost@live.com>
2020-12-13 18:42:45 +1030

    tidy

--- 13.py
+++ 13.py
@@ -1,24 +1,14 @@
-import math
-
 lines = [line.strip() for line in open('in/13.txt')]
 timestamp = int(lines[0])
-times = lines[1].split(',')
-buses = [(i, int(t)) for i,t in enumerate(times) if t != 'x']
+buses = [(i, int(t)) for i,t in enumerate(lines[1].split(',')) if t != 'x']
 
-lowest = 888888
-busId = -1
-for i, t in buses:
-    above = math.ceil(timestamp / t) * t
-    diff = above - timestamp
-    if diff < lowest:
-        lowest = diff
-        busId = t
-
-print(busId * lowest)
+times = [(time, (time - timestamp) % time) for i, time in buses]
+timeForLowest, lowestDifference = min(times, key=lambda x: x[1])
+print('part1:', timeForLowest * lowestDifference)
 
 result, product = 0,1
 for i,time in buses:
     while (result + i) % time != 0:
         result += product
     product *= time
-print(result)
+print('part2:', result)
