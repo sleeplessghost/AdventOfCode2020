dbf65c22ce59f8ca2a8eb84a599dafaa5adb2530
sleeplessghost <sleeplessghost@live.com>
2020-12-14 22:54:28 +1030

    tidy p2 and join it into p1 loop with separate memorys

--- 14.py
+++ 14.py
@@ -1,8 +1,8 @@
 import re
 
-def maskValue(value: str, mask: str):
-    value = int(value) | int(mask.replace('X', '0'), 2)
-    return value & int(mask.replace('X', '1'), 2)
+def maskValue(value: int, mask: str):
+    result = value | int(mask.replace('X', '0'), 2)
+    return result & int(mask.replace('X', '1'), 2)
 
 def getPermutations(binary: str):
     if 'X' not in binary:
@@ -13,32 +13,19 @@ def getPermutations(binary: str):
 
 lines = [line.strip() for line in open('in/14.txt')]
 
-mask = None
-mem = {}
+mask = ''
+mem1, mem2 = {}, {}
 for line in lines:
     if line.startswith('mask'):
         mask = line.split(' = ')[1]
     elif line.startswith('mem'):
         address, value = re.match(r'mem\[(\d+)\] = (\d+)', line).groups()
-        mem[address] = maskValue(value, mask)
-
-print('part1:', sum(mem.values()))
-
-mask = None
-mem = {}
-for line in lines:
-    if line.startswith('mask'):
-        mask = line.split(' = ')[1]
-    elif line.startswith('mem'):
-        address, value = re.match(r'mem\[(\d+)\] = (\d+)', line).groups()
-        binary = str(bin(int(address))).replace('0b','')
-        lengthDiff = len(mask) - len(binary)
-        binary = list(lengthDiff * '0' + binary)
-        for i, c in enumerate(mask):
-            if c == 'X' or c == '1':
-                binary[i] = c
-        binary = ''.join(binary)
+        value = int(value)
+        mem1[address] = maskValue(value, mask)
+        binary = str(bin(int(address))).replace('0b','').zfill(len(mask))
+        binary = ''.join([mask[i] if mask[i] != '0' else binary[i] for i in range(len(mask))])
         for addr in getPermutations(binary):
-            mem[addr] = int(value)
+            mem2[addr] = value
 
-print('part2:', sum(mem.values()))
+print('part1:', sum(mem1.values()))
+print('part2:', sum(mem2.values()))
