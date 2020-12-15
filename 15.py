22a8d37392503e9d1e0ddbbf3d69149380b59544
sleeplessghost <sleeplessghost@live.com>
2020-12-15 22:22:35 +1030

    tidy

--- 15.py
+++ 15.py
@@ -1,20 +1,20 @@
+def runToNumber(numbers, target):
+    mapped = {number: [index] for index, number in enumerate(numbers)}
+    lastNumber, count = numbers[-1], len(numbers)
+    while count < target:
+        prev = mapped[lastNumber]
+        if len(prev) < 2:
+            lastNumber = 0
+        else:
+            secondMost, recent = prev[-2:]
+            lastNumber = recent - secondMost
+        if lastNumber not in mapped:
+            mapped[lastNumber] = []
+        mapped[lastNumber].append(count)
+        count += 1
+    return lastNumber
+
 numbers = [int(n) for n in open('in/15.txt').read().split(',')]
 
-mapped = {}
-for i, n in enumerate(numbers):
-    mapped[n] = [i]
-
-lastNumber = numbers[-1]
-count = len(numbers)
-while count < 30000000:
-    prev = mapped[lastNumber]
-    if len(prev) < 2:
-        lastNumber = 0
-    else:
-        first, second = prev[-2:]
-        lastNumber = second - first
-    if lastNumber not in mapped:
-        mapped[lastNumber] = []
-    mapped[lastNumber].append(count)
-    count += 1
-print(count, ':', lastNumber)
+print('part1:', runToNumber(numbers, 2020))
+print('part2:', runToNumber(numbers, 30000000))
