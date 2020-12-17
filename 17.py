c03e0ff1367c41e60dc6c180530bb12bdd8160a9
sleeplessghost <sleeplessghost@live.com>
2020-12-17 18:29:51 +1030

    tidy some more

--- 17.py
+++ 17.py
@@ -3,13 +3,9 @@ from itertools import product
 from collections import defaultdict
 
 def activeNeighbours(cube, coords):
-    count = 0
     possibles = [[c-1,c,c+1] for c in coords]
     combinations = product(*possibles)
-    for coordinates in combinations:
-        if coordinates != coords:
-            count += cube[coordinates]
-    return count
+    return sum(cube[c] for c in combinations if c != coords)
 
 def step(oldCube, scaleMin, scaleMax, dimensions):
     cube = deepcopy(oldCube)
@@ -20,20 +16,17 @@ def step(oldCube, scaleMin, scaleMax, dimensions):
         else: cube[coords] = neighbours == 3
     return cube
 
+def calcCube(inp, dimensions, depth):
+    cube = defaultdict(bool)
+    for y, line in enumerate(inp):
+        for x, char in enumerate(line):
+            coords = list((x,y))
+            for __ in range(dimensions - 2): coords.append(0)
+            cube[tuple(coords)] = True if char == '#' else False
+    for i in range(depth):
+        cube = step(cube, 0 - i - 1, len(inp) + i + 1, dimensions)
+    return cube
+
 inp = [list(line.strip()) for line in open('in/17.txt')]
-
-cube = defaultdict(bool)
-for y, line in enumerate(inp):
-    for x, char in enumerate(line):
-        cube[(x,y,0)] = True if char == '#' else False
-
-for i in range(6): cube = step(cube, 0 - i - 1, len(inp) + i + 1, 3)
-print('part1:', sum(cube.values()))
-
-cube = defaultdict(bool)
-for y, line in enumerate(inp):
-    for x, char in enumerate(line):
-        cube[(x,y,0,0)] = True if char == '#' else False
-
-for i in range(6): cube = step(cube, 0 - i - 1, len(inp) + i + 1, 4)
-print('part2:', sum(cube.values()))
+print('part1:', sum(calcCube(inp, 3, 6).values()))
+print('part2:', sum(calcCube(inp, 4, 6).values()))
