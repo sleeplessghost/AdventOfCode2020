83aa2197a4fce0644c63d07406e31d1ebc7bdff4
sleeplessghost <sleeplessghost@live.com>
2020-12-17 17:04:49 +1030

    combine to 1 file and switch to defaultdict

--- 17.py
+++ 17.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from itertools import product
+from collections import defaultdict
 
 def activeNeighbours(cube, x, y, z):
     count = 0
@@ -7,36 +8,70 @@ def activeNeighbours(cube, x, y, z):
     combinations = product(*possibles)
     for cx, cy, cz in combinations:
         if not (cx == x and cy == y and cz == z):
-            if 0 <= cx < size and 0 <= cy < size and 0 <= cz < size:
-                count += cube[cx][cy][cz]
+            count += cube[cx][cy][cz]
     return count
 
-def step(oldCube):
+def activeNeighbours2(cube, x, y, z, w):
+    count = 0
+    possibles = [[x-1,x,x+1], [y-1,y,y+1], [z-1,z,z+1], [w-1,w,w+1]]
+    combinations = product(*possibles)
+    for cx, cy, cz, cw in combinations:
+        if not (cx == x and cy == y and cz == z and cw == w):
+            count += cube[cx][cy][cz][cw]
+    return count
+
+def step(oldCube, scaleMin, scaleMax):
     cube = deepcopy(oldCube)
-    for x in range(len(cube)):
-        for y in range(len(cube[x])):
-            for z in range(len(cube[x][y])):
+    for x in range(scaleMin, scaleMax):
+        for y in range(scaleMin, scaleMax):
+            for z in range(scaleMin, scaleMax):
                 neighbours = activeNeighbours(oldCube, x, y, z)
                 if oldCube[x][y][z]: cube[x][y][z] = 2 <= neighbours <= 3
                 else: cube[x][y][z] = neighbours == 3
     return cube
 
-inp = [list(line.strip()) for line in open('in/17.txt')]
-size = 30
-offset = 12
-zoffset = 15
-cube = [[[False for x in range(size)] for y in range(size)] for z in range(size)]
+def step2(oldCube, scaleMin, scaleMax):
+    cube = deepcopy(oldCube)
+    for x in range(scaleMin, scaleMax):
+        for y in range(scaleMin, scaleMax):
+            for z in range(scaleMin, scaleMax):
+                for w in range(scaleMin, scaleMax):
+                    neighbours = activeNeighbours2(oldCube, x, y, z, w)
+                    if oldCube[x][y][z][w]: cube[x][y][z][w] = 2 <= neighbours <= 3
+                    else: cube[x][y][z][w] = neighbours == 3
+    return cube
 
-for i, line in enumerate(inp):
-    for j, char in enumerate(line):
-        cube[offset + j][offset + i][zoffset] = True if char == '#' else False
+inp = [list(line.strip()) for line in open('in/17.txt')]
+size = len(inp)
+cube = defaultdict(lambda: defaultdict(lambda: defaultdict(bool)))
+
+for y, line in enumerate(inp):
+    for x, char in enumerate(line):
+        cube[x][y][0] = True if char == '#' else False
 
 for i in range(6):
-    cube = step(cube)
+    cube = step(cube, 0 - i - 1, size + i + 1)
 
 result = 0
-for x in range(len(cube)):
-    for y in range(len(cube[x])):
-        for z in range(len(cube[x][y])):
+for x in cube:
+    for y in cube[x]:
+        for z in cube[x][y]:
             result += cube[x][y][z]
-print(result)
+print('part1:', result)
+
+cube = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(bool))))
+
+for y, line in enumerate(inp):
+    for x, char in enumerate(line):
+        cube[x][y][0][0] = True if char == '#' else False
+
+for i in range(6):
+    cube = step2(cube, 0 - i - 1, size + i + 1)
+
+result = 0
+for x in cube:
+    for y in cube[x]:
+        for z in cube[x][y]:
+            for w in cube[x][y][z]:
+                result += cube[x][y][z][w]
+print('part2:', result)
