5a47969a1111e643fec9a38a2fd14000a86898a0
sleeplessghost <sleeplessghost@live.com>
2020-12-19 03:49:40 +1030

    tidy

--- 18.py
+++ 18.py
@@ -1,43 +1,31 @@
 import re
 
-def op(total, value, operator):
-    if operator == '+': return total + value
-    elif operator == '-': return total - value
-    elif operator == '*': return total * value
-
-def evaluate(expr):
-    while '(' in expr:
-        start = expr.find('(') + 1
-        end = 0
-        sub = expr[start:]
-        count = 1
-        for i, c in enumerate(sub):
-            if c == '(': count += 1
-            elif c == ')': count -= 1
-            if count == 0: 
-                end = i
-                break
-        sub = sub[:end]
-        end += start
-        brack = evaluate(sub)
-        expr = expr[:start-1] + str(brack) + expr[end+1:]
-    result = None
-    operator = '+'
-    while '+' in expr:
-        matches = re.findall(r'(\d+ \+ \d+)', expr)
-        if matches is None: break
-        match = matches[0]
-        split = match.split(' + ')
-        evald = int(split[0]) + int(split[1])
-        start = expr.find(match)
-        expr = expr[:start] + str(evald) + expr[start + len(match):]
+def ltr(expr):
+    result, operator = None, None
     for token in expr.split(' '):
-            if token in ['+','-','*']: operator = token
-            elif result is None: result = int(token)
-            else: result = op(result, int(token), operator)
+        if token in ['+','*']: operator = token
+        elif result is None: result = int(token)
+        elif operator == '+': result += int(token)
+        elif operator == '*': result *= int(token)
     return result
+
+def additions(expr):
+    match = re.search(r'(\d+ \+ \d+)', expr)
+    while match:
+        m = match.group()
+        expr = expr.replace(m, str(ltr(m)), 1)
+        match = re.search(r'(\d+ \+ \d+)', expr)
+    return ltr(expr)
+
+def evaluate(expr, additionFirst):
+    while '(' in expr:
+        matches = re.findall(r'(\([0-9\+\*\s]+\))', expr)
+        for match in matches:
+            if additionFirst: expr = expr.replace(match, str(additions(match[1:-1])))
+            else: expr = expr.replace(match, str(ltr(match[1:-1])))
+    return additions(expr) if additionFirst else ltr(expr)
 
 lines = [line.strip() for line in open('in/18.txt')]
 
-result = sum(evaluate(line) for line in lines)
-print('part2:', result)
+print('part1:', sum(evaluate(line, False) for line in lines))
+print('part2:', sum(evaluate(line, True) for line in lines))
