49a7c03f605bbb8204f43a66138f1d9e7ebe6993
sleeplessghost <sleeplessghost@live.com>
2020-12-22 16:02:55 +1030

    day22

---
+++ 22.py
@@ -0,0 +1,54 @@
+def parseDeck(deck):
+    return [int(n.strip()) for n in deck.splitlines()[1:]]
+
+def anyPreviousState(deck, states):
+    for state in states:
+        if len(deck) == len(state) and all(deck[i] == n for i,n in enumerate(state)): return True
+    return False
+
+def recursiveCombat(p1, p2):
+    previousP1 = []
+    previousP2 = []
+    while len(p1) > 0 and len(p2) > 0:
+        if anyPreviousState(p1, previousP1) and anyPreviousState(p2, previousP2):
+            return 1, p1
+        else:
+            previousP1.append(p1.copy())
+            previousP2.append(p2.copy())
+            card1 = p1.pop(0)
+            card2 = p2.pop(0)
+            if card1 <= len(p1) and card2 <= len(p2):
+                number, __ = recursiveCombat(p1[:card1], p2[:card2])
+                winner = p1 if number == 1 else p2
+                winnerCard = card1 if number == 1 else card2
+                loserCard = card2 if number == 1 else card1
+                winner.append(winnerCard)
+                winner.append(loserCard)
+            else:
+                winner = p1 if card1 > card2 else p2
+                winnerCard = max(card1, card2)
+                loserCard = min(card1, card2)
+                winner.append(winnerCard)
+                winner.append(loserCard)
+    number = 1 if len(p1) > 0 else 2
+    winner = p1 if len(p1) > 0 else p2
+    return number, winner
+
+players = open('in/22.txt').read().split('\n\n')
+
+p1, p2 = [parseDeck(p) for p in players]
+
+while len(p1) > 0 and len(p2) > 0:
+    card1 = p1.pop(0)
+    card2 = p2.pop(0)
+    winner = p1 if card1 > card2 else p2
+    winnerCard = max(card1, card2)
+    loserCard = min(card1, card2)
+    winner.append(winnerCard)
+    winner.append(loserCard)
+winner = p1 if len(p1) > 0 else p2
+print('part1:', sum((i+1) * n for i,n in enumerate(winner[::-1])))
+
+p1, p2 = [parseDeck(p) for p in players]
+number, winner = recursiveCombat(p1, p2)
+print('part2:', sum((i+1) * n for i,n in enumerate(winner[::-1])))
