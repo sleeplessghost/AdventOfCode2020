4878212a75bf3b37e8a0d5d9b86099d5388cd63e
sleeplessghost <sleeplessghost@live.com>
2020-12-22 16:32:07 +1030

    tidy

--- 22.py
+++ 22.py
@@ -6,49 +6,37 @@ def anyPreviousState(deck, states):
 
 def combat(p1, p2):
     while len(p1) > 0 and len(p2) > 0:
-        card1 = p1.pop(0)
-        card2 = p2.pop(0)
-        winner = p1 if card1 > card2 else p2
-        winnerCard = max(card1, card2)
-        loserCard = min(card1, card2)
+        card1, card2 = p1.pop(0), p2.pop(0)
+        winner, winnerCard, loserCard = (p1, card1, card2) if card1 > card2 else (p2, card2, card1)
         winner.append(winnerCard)
         winner.append(loserCard)
     return p1 if len(p1) > 0 else p2
 
 def recursiveCombat(p1, p2):
-    previousP1 = []
-    previousP2 = []
+    previousP1, previousP2 = [], []
     while len(p1) > 0 and len(p2) > 0:
         if anyPreviousState(p1, previousP1) and anyPreviousState(p2, previousP2):
             return 1, p1
         else:
             previousP1.append(tuple(p1))
             previousP2.append(tuple(p2))
-            card1 = p1.pop(0)
-            card2 = p2.pop(0)
+            card1, card2 = p1.pop(0), p2.pop(0)
             if card1 <= len(p1) and card2 <= len(p2):
                 number, __ = recursiveCombat(p1[:card1], p2[:card2])
-                winner = p1 if number == 1 else p2
-                winnerCard = card1 if number == 1 else card2
-                loserCard = card2 if number == 1 else card1
-                winner.append(winnerCard)
-                winner.append(loserCard)
+                winner, winnerCard, loserCard = (p1, card1, card2) if number == 1 else (p2, card2, card1)
             else:
-                winner = p1 if card1 > card2 else p2
-                winnerCard = max(card1, card2)
-                loserCard = min(card1, card2)
-                winner.append(winnerCard)
-                winner.append(loserCard)
-    number = 1 if len(p1) > 0 else 2
-    winner = p1 if len(p1) > 0 else p2
+                winner, winnerCard, loserCard = (p1, card1, card2) if card1 > card2 else (p2, card2, card1)
+            winner.append(winnerCard)
+            winner.append(loserCard)
+    number, winner = (1, p1) if len(p1) > 0 else (2, p2)
     return number, winner
 
 decks = open('in/22.txt').read().split('\n\n')
 
 p1, p2 = [parseDeck(d) for d in decks]
 winner = combat(p1, p2)
-print('part1:', sum((i+1) * n for i,n in enumerate(winner[::-1])))
+print('part1:', sum(i*n for i,n in enumerate(winner[::-1], 1)))
 
 p1, p2 = [parseDeck(d) for d in decks]
-number, winner = recursiveCombat(p1, p2)
-print('part2:', sum((i+1) * n for i,n in enumerate(winner[::-1])))
+__, winner = recursiveCombat(p1, p2)
+print('part2:', sum(i*n for i,n in enumerate(winner[::-1], 1)))
